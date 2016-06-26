91bad4ce02d1904df94eee444b2ae17d355a8eb3
Patrick Graham <pabreetzio@gmail.com>
2016-06-25 23:09:45 -0400

    Adding Fody

---
+++ GameClock/GameClock/ViewModel/GameViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameClock.Model;
+using PropertyChanged;
+
+namespace GameClock.ViewModel
+{   [ImplementPropertyChanged]
+    class GameViewModel
+    {
+
+    }
+}
